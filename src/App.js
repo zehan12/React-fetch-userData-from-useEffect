d8e9dc53e4466e8e42ac43722f97e4c7eef538b0
Zehan Khan <73664886+zehan12@users.noreply.github.com>
2022-10-04 17:52:24 +0530

    expand details

--- src/App.js
+++ src/App.js
@@ -16,21 +16,38 @@ export default function App() {
     const res = await fetch(url);
     const { results } = await res.json();
     setData(results[0]);
+    console.log(data);
     setLoading(false);
   };
 
   return (
     <>
+      <h2>
+        <button
+          style={{
+            backgroundColor: "lime",
+            height: "60px",
+            fontSize: "30px",
+            fontWeight: "900"
+          }}
+          onClick={() => fetchData(userApi)}
+        >
+          Click for New User
+        </button>
+      </h2>
       {loading && <h1>Loading....</h1>}
       {data.length !== 0 ? (
         <div>
           <h1>{data.name.first + "  " + data.name.last}</h1>
-          <img src={data.picture.medium} alt={data.name} />
+          <img src={data.picture.large} alt={data.name} />
           <p>Gender: {data.gender} </p>
           <p>DOB: {data.dob.date} </p>
           <p>Age: {data.dob.age} </p>
-          <p>Loacation: {data.location.country} </p>
-          <button onClick={() => fetchData(userApi)}>New User</button>
+          <p>City: {data.location.city} </p>
+          <p>State: {data.location.state} </p>
+          <p>Country: {data.location.country} </p>
+          <p>Phone: {data.phone} </p>
+          <p>Email: {data.email} </p>
         </div>
       ) : (
         <h1>Error in Fetch</h1>
